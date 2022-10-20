8124f2918a5b93aa020f709062a060a1eda79c61
Milena Porto Coyado <116229101+micoyado@users.noreply.github.com>
2022-10-20 00:53:18 +0000

    aprendi a criar variáveis e exibi-las na tela! :)

--- main.py
+++ main.py
@@ -8,4 +8,12 @@ print ("Hello, World!")
 print ("Hello, World!")
 print ("Hello, World!")
 
+# quando quiser guardar uma string (frase)
+nome = "Milena Porto"
+
+# quando quiser guardar um número inteiro
+idade = 18  
+
+# exibir o que está dentro da variável nome
+print ("meu nominho é " + nome)
 
