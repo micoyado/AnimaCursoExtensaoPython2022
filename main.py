507330eacb1027c589a6ff13366e29551e19ec7b
Milena Porto Coyado <116229101+micoyado@users.noreply.github.com>
2022-10-20 00:33:12 +0000

    adicionei o alô mundo :)

---
+++ main.py
@@ -0,0 +1,11 @@
+# para comentar em linha
+
+# print() = comando de saída
+print ("Hello, World!")
+print ("Hello, World!")
+print ("Hello, World!")
+print ("Hello, World!")
+print ("Hello, World!")
+print ("Hello, World!")
+
+
